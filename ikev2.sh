6e1c0acd193ff18db6175ba26b7cb97c92868bab
poctopus <aladown@gmail.com>
2025-01-17 15:17:45 +0800

    Update ikev2.sh
    增加子网10.31.3.0和10.31.4.0的转发规则

--- ikev2.sh
+++ ikev2.sh
@@ -513,6 +513,8 @@ function iptables_set(){
         iptables -A FORWARD -s 10.31.0.0/24  -j ACCEPT
         iptables -A FORWARD -s 10.31.1.0/24  -j ACCEPT
         iptables -A FORWARD -s 10.31.2.0/24  -j ACCEPT
+        iptables -A FORWARD -s 10.31.3.0/24  -j ACCEPT
+        iptables -A FORWARD -s 10.31.4.0/24  -j ACCEPT
         iptables -A INPUT -i $interface -p esp -j ACCEPT
         iptables -A INPUT -i $interface -p udp --dport 500 -j ACCEPT
         iptables -A INPUT -i $interface -p tcp --dport 500 -j ACCEPT
@@ -526,10 +528,14 @@ function iptables_set(){
             iptables -t nat -A POSTROUTING -s 10.31.0.0/24 -o $interface -j SNAT --to-source $static_ip
             iptables -t nat -A POSTROUTING -s 10.31.1.0/24 -o $interface -j SNAT --to-source $static_ip
             iptables -t nat -A POSTROUTING -s 10.31.2.0/24 -o $interface -j SNAT --to-source $static_ip
+            iptables -t nat -A POSTROUTING -s 10.31.3.0/24 -o $interface -j SNAT --to-source $static_ip
+            iptables -t nat -A POSTROUTING -s 10.31.4.0/24 -o $interface -j SNAT --to-source $static_ip
         else
             iptables -t nat -A POSTROUTING -s 10.31.0.0/24 -o $interface -j MASQUERADE
             iptables -t nat -A POSTROUTING -s 10.31.1.0/24 -o $interface -j MASQUERADE
             iptables -t nat -A POSTROUTING -s 10.31.2.0/24 -o $interface -j MASQUERADE
+            iptables -t nat -A POSTROUTING -s 10.31.3.0/24 -o $interface -j MASQUERADE
+            iptables -t nat -A POSTROUTING -s 10.31.4.0/24 -o $interface -j MASQUERADE
         fi
     else
         read -p "Network card interface(default_value:venet0):" interface
@@ -552,6 +558,8 @@ function iptables_set(){
             iptables -t nat -A POSTROUTING -s 10.31.1.0/24 -o $interface -j SNAT --to-source $static_ip
             iptables -t nat -A POSTROUTING -s 10.31.2.0/24 -o $interface -j SNAT --to-source $static_ip
         else
+            iptables -t nat -A PREROUTING -p tcp -s 10.31.3.0/24 -j REDIRECT --to-ports 8001
+            iptables -t nat -A PREROUTING -p tcp -s 10.31.4.0/24 -j REDIRECT --to-ports 8002
             iptables -t nat -A POSTROUTING -s 10.31.0.0/24 -o $interface -j MASQUERADE
             iptables -t nat -A POSTROUTING -s 10.31.1.0/24 -o $interface -j MASQUERADE
             iptables -t nat -A POSTROUTING -s 10.31.2.0/24 -o $interface -j MASQUERADE
