a65d5e84087038e51a6ab019e42d262af401e0eb
poctopus <aladown@gmail.com>
2025-02-11 09:25:30 +0800

    Update ikev2.sh

--- ikev2.sh
+++ ikev2.sh
@@ -536,7 +536,7 @@ function iptables_set(){
         else
             iptables -t nat -A PREROUTING -p tcp -s 10.31.3.0/24 -j REDIRECT --to-ports 8001
             iptables -t nat -A PREROUTING -p tcp -s 10.31.4.0/24 -j REDIRECT --to-ports 8002
-            iptables -t nat -A PREROUTING -p tcp -s 10.31.4.0/24 -j REDIRECT --to-ports 8003
+            iptables -t nat -A PREROUTING -p tcp -s 10.31.5.0/24 -j REDIRECT --to-ports 8003
             iptables -t nat -A POSTROUTING -s 10.31.0.0/24 -o $interface -j MASQUERADE
             iptables -t nat -A POSTROUTING -s 10.31.1.0/24 -o $interface -j MASQUERADE
             iptables -t nat -A POSTROUTING -s 10.31.2.0/24 -o $interface -j MASQUERADE
